52ec8b2ff1d1fba8f3a8509dfbe66a3bb54ff38d
janbrascamp <j.w.brascamp@gmail.com>
2024-02-20 09:04:18 -0500

    Skipping subject in run_mattar_analysis_on_MSU_face_data if output folder is already present

--- mattar_analysis/run_mattar_analysis_on_MSU_face_data.m
+++ mattar_analysis/run_mattar_analysis_on_MSU_face_data.m
@@ -60,122 +60,123 @@ for subject_index = 1:length(subject_IDs)
 
 	% Place to save the results files
 	this_output_data_path = convertStringsToChars(fullfile(output_data_path,subject_ID,'ses-1','func'));
-	if ~isfolder(this_output_data_path)
-	    mkdir(this_output_data_path);
-	end
+	
+	if ~isfolder(this_output_data_path)	%only continue analysis if the output folder doesn't exist yet
 
-	% Paths and filenames for the input data: provide the paths and collect all files there that have the right name pattern
-	% First the functional data, then the event data
+		mkdir(this_output_data_path);
 	
-	this_input_path_func_data = convertStringsToChars(fullfile(input_path_func_data,subject_ID,'ses-1','func'));
-	this_input_path_event_data = convertStringsToChars(fullfile(input_path_event_data,subject_ID,'ses-1','func'));
+		% Paths and filenames for the input data: provide the paths and collect all files there that have the right name pattern
+		% First the functional data, then the event data
 	
-	these_input_paths={this_input_path_func_data, this_input_path_event_data};
-	file_name_search_strings={'space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz','_events.tsv'};	%strings to search for within file names to see whether file in question is input file we're interested in
+		this_input_path_func_data = convertStringsToChars(fullfile(input_path_func_data,subject_ID,'ses-1','func'));
+		this_input_path_event_data = convertStringsToChars(fullfile(input_path_event_data,subject_ID,'ses-1','func'));
 	
-	for func_event_toggle=1:2
+		these_input_paths={this_input_path_func_data, this_input_path_event_data};
+		file_name_search_strings={'space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz','_events.tsv'};	%strings to search for within file names to see whether file in question is input file we're interested in
 	
-		this_input_path=string(these_input_paths(func_event_toggle));
-		this_file_name_search_string=string(file_name_search_strings(func_event_toggle));
+		for func_event_toggle=1:2
+	
+			this_input_path=string(these_input_paths(func_event_toggle));
+			this_file_name_search_string=string(file_name_search_strings(func_event_toggle));
 		
-		input_folder_contents=dir(this_input_path);
+			input_folder_contents=dir(this_input_path);
 	
-		file_names={};
+			file_names={};
 	
-		for file_or_folder_counter = 1:length(input_folder_contents)
-			this_file_or_folder=input_folder_contents(file_or_folder_counter);
-			if ~isempty(strfind(this_file_or_folder.name,this_file_name_search_string)) & isempty(strfind(this_file_or_folder.name,'._sub-'))
-				file_names=[file_names,this_file_or_folder.name];
+			for file_or_folder_counter = 1:length(input_folder_contents)
+				this_file_or_folder=input_folder_contents(file_or_folder_counter);
+				if ~isempty(strfind(this_file_or_folder.name,this_file_name_search_string)) & isempty(strfind(this_file_or_folder.name,'._sub-'))
+					file_names=[file_names,this_file_or_folder.name];
+				end
+			end
+		
+			if func_event_toggle==1
+				func_file_names=sort(file_names);
+			else
+				event_file_names=sort(file_names);
 			end
 		end
-		
-		if func_event_toggle==1
-			func_file_names=sort(file_names);
+	
+		% Get the stimulus files from the event file content.
+		[stimulus,stimTime] = parseEventFiles(this_input_path_event_data,event_file_names);
+	
+		% Get the functional data files
+		[data,templateImage] = parseDataFiles(this_input_path_func_data,func_file_names,smooth_sd);
+	
+		% Pick the voxels to analyze
+		xyz = templateImage.volsize;
+		if fit_one_voxel
+		    % A single voxel that is in the right FFA
+		    vxs = 83793;
+		    averageVoxels = true;
 		else
-			event_file_names=sort(file_names);
+		    % Create a mask of brain voxels
+		    brainThresh = 2000;
+		    vxs = find(reshape(templateImage.vol, [prod(xyz), 1]) > brainThresh);
+		    averageVoxels = false;
 		end
-	end
-	
-	% Get the stimulus files from the event file content.
-	[stimulus,stimTime] = parseEventFiles(this_input_path_event_data,event_file_names);
-	
-	% Get the functional data files
-	[data,templateImage] = parseDataFiles(this_input_path_func_data,func_file_names,smooth_sd);
-	
-	% Pick the voxels to analyze
-	xyz = templateImage.volsize;
-	if fit_one_voxel
-	    % A single voxel that is in the right FFA
-	    vxs = 83793;
-	    averageVoxels = true;
-	else
-	    % Create a mask of brain voxels
-	    brainThresh = 2000;
-	    vxs = find(reshape(templateImage.vol, [prod(xyz), 1]) > brainThresh);
-	    averageVoxels = false;
-	end
 
-	% Create the model opts, which includes stimLabels and typicalGain. The
-	% paraSD key-value controls how varied the HRF solutions can be. A value of
-	% 3 is fairly conservative and will keep the HRFs close to a canonical
-	% shape. This is necessary for the current experiment as the stimulus
-	% sequence does not uniquely constrain the temporal delay in the HRF.
-	stimLabels = cellfun(@(x) sprintf('face_%02d',str2double(string(x))),num2cell(1:nFaces),'UniformOutput',false);
-	stimLabels = [stimLabels,'firstFace','repeatFace','right-left'];
-	modelOpts = {'stimLabels',stimLabels,'typicalGain',typical_gain,'paraSD',3,'polyDeg',poly_deg};
+		% Create the model opts, which includes stimLabels and typicalGain. The
+		% paraSD key-value controls how varied the HRF solutions can be. A value of
+		% 3 is fairly conservative and will keep the HRFs close to a canonical
+		% shape. This is necessary for the current experiment as the stimulus
+		% sequence does not uniquely constrain the temporal delay in the HRF.
+		stimLabels = cellfun(@(x) sprintf('face_%02d',str2double(string(x))),num2cell(1:nFaces),'UniformOutput',false);
+		stimLabels = [stimLabels,'firstFace','repeatFace','right-left'];
+		modelOpts = {'stimLabels',stimLabels,'typicalGain',typical_gain,'paraSD',3,'polyDeg',poly_deg};
 
-	% Define the modelClass
-	modelClass = 'mattarAdapt';
+		% Define the modelClass
+		modelClass = 'mattarAdapt';
 
-	% Call the forwardModel
-	results = forwardModel(data,stimulus,tr,...
-	    'stimTime',stimTime,...
-	    'vxs',vxs,...
-	    'averageVoxels',averageVoxels,...
-	    'verbose',true,...
-	    'modelClass',modelClass,...
-	    'modelOpts',modelOpts);
+		% Call the forwardModel
+		results = forwardModel(data,stimulus,tr,...
+		    'stimTime',stimTime,...
+		    'vxs',vxs,...
+		    'averageVoxels',averageVoxels,...
+		    'verbose',true,...
+		    'modelClass',modelClass,...
+		    'modelOpts',modelOpts);
 
-	% Show the results figures
-	figFields = fieldnames(results.figures);
-	if ~isempty(figFields)
-	    for ii = 1:length(figFields)
-	        figHandle = struct2handle(results.figures.(figFields{ii}).hgS_070000,0,'convert');
-	        figHandle.Visible = 'on';
-	    end
-	end
+		% Show the results figures
+		figFields = fieldnames(results.figures);
+		if ~isempty(figFields)
+		    for ii = 1:length(figFields)
+		        figHandle = struct2handle(results.figures.(figFields{ii}).hgS_070000,0,'convert');
+		        figHandle.Visible = 'on';
+		    end
+		end
 
-	% Save some files if we processed the whole brain
-	if ~fit_one_voxel
-		file_name_stem=[convertStringsToChars(subject_ID) '_task-main_space-MNI152NLin2009cAsym_desc-']
+		% Save some files if we processed the whole brain
+		if ~fit_one_voxel
+			file_name_stem=[convertStringsToChars(subject_ID) '_task-main_space-MNI152NLin2009cAsym_desc-']
 		
-	    % Save the results
-	    fileName = fullfile(this_output_data_path,[file_name_stem 'mattarAdaptResults.mat']);
-	    save(fileName,'results');
+		    % Save the results
+		    fileName = fullfile(this_output_data_path,[file_name_stem 'mattarAdaptResults.mat']);
+		    save(fileName,'results');
 
-	    % Save the template image
-	    fileName = fullfile(this_output_data_path,[file_name_stem 'epiTemplate.nii']);
-	    MRIwrite(templateImage, fileName);
+		    % Save the template image
+		    fileName = fullfile(this_output_data_path,[file_name_stem 'epiTemplate.nii']);
+		    MRIwrite(templateImage, fileName);
 
-	    % Save a map of R2 values
-	    newImage = templateImage;
-	    volVec = results.R2;
-	    volVec(isnan(volVec)) = 0;
-	    newImage.vol = reshape(volVec,xyz(1),xyz(2),xyz(3));
-	    fileName = fullfile(this_output_data_path,[file_name_stem 'mattarAdapt_R2.nii']);
-	    MRIwrite(newImage, fileName);
+		    % Save a map of R2 values
+		    newImage = templateImage;
+		    volVec = results.R2;
+		    volVec(isnan(volVec)) = 0;
+		    newImage.vol = reshape(volVec,xyz(1),xyz(2),xyz(3));
+		    fileName = fullfile(this_output_data_path,[file_name_stem 'mattarAdapt_R2.nii']);
+		    MRIwrite(newImage, fileName);
 
-	    % Save maps for the various param vals
-	    paramLabels = {'firstFace','repeatFace','right-left','adaptMu','adaptGain'};
-	    paramIdx = nFaces+1:nFaces+length(paramLabels);
-	    for ii = 1:length(paramLabels)
-	        newImage = templateImage;
-	        volVec = results.params(:,paramIdx(ii));
-	        volVec(isnan(volVec)) = 0;
-	        newImage.vol = reshape(volVec,xyz(1),xyz(2),xyz(3));
-	        fileName = fullfile(this_output_data_path,[file_name_stem '_mattarAdapt_' paramLabels{ii} '.nii']);
-	        MRIwrite(newImage, fileName);
-	    end
+		    % Save maps for the various param vals
+		    paramLabels = {'firstFace','repeatFace','right-left','adaptMu','adaptGain'};
+		    paramIdx = nFaces+1:nFaces+length(paramLabels);
+		    for ii = 1:length(paramLabels)
+		        newImage = templateImage;
+		        volVec = results.params(:,paramIdx(ii));
+		        volVec(isnan(volVec)) = 0;
+		        newImage.vol = reshape(volVec,xyz(1),xyz(2),xyz(3));
+		        fileName = fullfile(this_output_data_path,[file_name_stem '_mattarAdapt_' paramLabels{ii} '.nii']);
+		        MRIwrite(newImage, fileName);
+		    end
 
-	end
+		end
 end
